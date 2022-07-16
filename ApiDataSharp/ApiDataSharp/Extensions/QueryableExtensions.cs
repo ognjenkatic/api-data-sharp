c612a0f240d81c6f4aa7ed6c91d43ff1d5bfc226
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2022-07-16 13:30:12 +0200

    update formatting

--- ApiDataSharp/ApiDataSharp/Extensions/QueryableExtensions.cs
+++ ApiDataSharp/ApiDataSharp/Extensions/QueryableExtensions.cs
@@ -12,16 +12,13 @@ namespace ApiDataSharp.Extensions
         public static IQueryable<T> WhereDataSharp<T>(
             this IQueryable<T> queryable,
             IDataSharpRequest request
-        ) {
+        ) 
+        {
             if (request is IFilterRequest<T> filterRequest && filterRequest.Filter != null)
-            {
                 queryable = FilteringService.Filter(queryable, filterRequest);
-            }
 
             if (request is ISortRequest<T> sortRequest && sortRequest.SortBy != null)
-            {
                 queryable = SortingService.Sort(queryable, sortRequest);
-            }
 
             return queryable;
         }
